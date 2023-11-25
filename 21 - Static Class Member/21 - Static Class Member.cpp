d833a906a8c5e9687f09e8a4a4d1d4ac4a3d504f
rasuantaputra <rasuantaportofolio@gmail.com>
2023-11-26 00:04:01 +0700

    21 - Static Class Member

---
+++ 21 - Static Class Member/21 - Static Class Member.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Hero{
+    private:
+        string name;
+        double health;
+
+    public:
+        // "static" hanya hidup di level class
+        static int count;
+
+        Hero(const char *name, double health){
+            this->name = name;
+            this->health = health;
+
+            count++;
+        }
+
+        // destructor
+        ~Hero(){
+            count--;
+            cout << this->name <<" sudah di delete guys" << endl;
+        }
+
+        void display(){
+            cout << "name\t : " << this->name << endl;
+            cout << "health\t: " << this->health << endl;
+        }
+};
+
+int Hero::count = 0;
+
+int main(int argc, char const *argv[])
+{
+    Hero hero1 = Hero("Ucup", 100);
+    hero1.display();
+    cout << "jumlah object Hero adalah : " << hero1.count << endl;
+    cout << "jumlah object Hero dari class : " << Hero::count << endl;
+
+    Hero hero2 = Hero("otong", 100);
+    hero2.display();
+    cout << "\njumlah object Hero adalah : " << hero2.count << endl;
+    cout << "jumlah object Hero dari class : " << Hero::count << endl;
+
+    Hero *hero3 = new Hero("Dudung", 50);
+    hero3->display();
+    cout << "\njumlah object Hero adalah : " << hero3->count << endl;
+    cout << "jumlah object Hero dari class : " << Hero::count << endl;
+    
+    Hero *hero4 = new Hero("Blankai", 250);
+    hero4->display();
+    cout << "\njumlah object Hero adalah : " << hero4->count << endl;
+    cout << "jumlah object Hero dari class : " << Hero::count << endl;
+
+    delete hero3;
+    cout << "jumlah object Hero dari class : " << Hero::count << endl;
+    return 0;
+}
